608588c3129a24ab48f04bde732607da1b720bbe
LiamGunning07 <liamgunning07@gmail.com>
2023-12-13 10:44:20 -0700

    "Updated with functions to handle user input"

--- client.js
+++ client.js
@@ -1,5 +1,9 @@
 const net = require("net");
 
+const move = (conn, direction) => {
+  const command = `Move: ${direction}`;
+  conn.write(command);
+};
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
@@ -15,6 +19,9 @@ conn.on("connect",() => {
   console.log("Eat as many apples without crashing into walls, or yourself. Goodluck !");
 
   conn.write("Name:LJG");
+//  setTimeout(() => move(conn, 'up'), 100); // First move up
+//  setTimeout(() => move(conn, 'up'), 200); // Second move up
+ // setTimeout(() => move(conn, 'up'), 300); // Third move up
 });
 
 return conn;
@@ -22,4 +29,38 @@ return conn;
 };
 console.log("Connecting ...");
 
-module.exports = connect;
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const conn = connect()
+const stdin = setupInput();
+
+
+stdin.on("data", (key) => {
+  handleUserInput(key);
+});
+
+const handleUserInput = (key) => {
+  // Exit the game if the user presses Ctrl + C
+  if (key === '\u0003') {
+    process.exit();
+  }
+  const keyMap = {
+    w: 'up',
+    a: 'left',
+    s: 'down',
+    d: 'right'
+  };
+
+  if (key in keyMap) {
+    move(conn, keyMap[key]);
+  }
+};
+
+
+module.exports = { connect, move };
--- play.js
+++ play.js
@@ -1,7 +1,16 @@
 const net = require("net");
-const connect = require("./client")
+
+const { connect, move } = require("./client")
 
 const conn = connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
 
 conn.setEncoding("utf8"); // interpret data as text
 
