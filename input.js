a4f6cc2dde5b01e682b03828d958d41b81a37074
LiamGunning07 <liamgunning07@gmail.com>
2023-12-13 14:30:57 -0700

    "Updating before making changes"

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
 const { connect , move} =  require("./client");
+let connection;
 
 const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
